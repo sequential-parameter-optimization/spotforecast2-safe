fc2a015c80a96f772760f4b745007f7586dac7f8
bartzbeielstein <32470350+bartzbeielstein@users.noreply.github.com>
2026-02-08 11:01:50 +0100

    Create __init__.py

---
+++ src/spotforecast2_safe/downloader/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2026 bartzbeielstein
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from .entsoe import download_new_data, merge_build_manual
+
+__all__ = ["download_new_data", "merge_build_manual"]
